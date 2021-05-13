b5615a03c3b0da147aeb0eab36393f927cbf30ff
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-05-13 17:22:39 -0400

    ⚡️ Update: Save, Add, + Display task

--- iOS_Tasks/EntryViewController.swift
+++ iOS_Tasks/EntryViewController.swift
@@ -12,11 +12,17 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
     // User enters a task + done button function to save
     @IBOutlet var field: UITextField!
 
+    // Update tasks to refect new task
+    var update: (() -> Void)?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Prompt to save once done is selected
         field.delegate = self
+
+        // Save button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveTask))
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -24,20 +30,34 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
 
         return true
     }
-    @IBAction func saveTask() {
+    // @objc allows func to be used as #selector
+    @objc func saveTask() {
+        // If field is not empty, save content of input fields
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
 
+        // Save task using user default + count of task + task with number
+        // Guard againt count becoming nil
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+
+        let newCount = count + 1
+        UserDefaults().set(newCount, forKey: "count")
+
+        // Save task with updated count
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+
+        // Call func is update func exist
+        update?()
+
+        // Dismiss func
+        navigationController?.popViewController(animated: true)
     }
 }
 
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 
--- iOS_Tasks/ViewController.swift
+++ iOS_Tasks/ViewController.swift
@@ -16,19 +16,61 @@ class ViewController: UIViewController {
     var tasks = [String]()
 
     override func viewDidLoad() {
+        // Get all saved tasks
         super.viewDidLoad()
-        // Get all current saved tasks
+        self.title = "Tasks"
+
+        // Connect delegate
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        // Save task setup
+        // Check if no tasks, if true, given count value of 0
+        if !UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+        // Get current saved tasks
+        updateTasks()
+    }
+
+    func updateTasks() {
+
+        // Avoids duplicates
+        tasks.removeAll()
+
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+
+        // Iterate through 0 until count
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+                tasks.append(task)
+            }
+        }
+
+        // Reload table
+        tableView.reloadData()
+
     }
 
     // Once Add button is clicked/ tapped
     @IBAction func didTapAdd() {
+
         // To Add: Instantiate EntryViewController
         let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
         vc.title = "New Task"
+
+        // Prioritize refetch saved tasks
+        vc.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
         navigationController?.pushViewController(vc, animated: true)
     }
 }
-
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {

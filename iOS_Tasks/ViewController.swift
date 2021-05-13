6f5ea2a28e8845e84536b71c9cdd6e44baa8cc19
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-05-13 15:06:26 -0400

    🎉 Update: Build simulator + View Controls connect to tableView

--- iOS_Tasks/ViewController.swift
+++ iOS_Tasks/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Control table view on storyboard
+    @IBOutlet var tableView: UITableView!
+
+    // Array holds all tasks created by user
+    var tasks = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Get all current saved tasks
     }
 
 
 }
 
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// Implement functions to return number of rows from tasks array
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    // Does not return if there is tasks string is empty
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Position of cell in table
+        cell.textLabel?.text = tasks[indexPath.row]
+
+        return cell
+    }
+}

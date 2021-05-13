adf4ad9278a1831a425a4fe77581c37a194c8585
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-05-13 17:44:04 -0400

    ⚡️Add: Task Delete view

--- iOS_Tasks/ViewController.swift
+++ iOS_Tasks/ViewController.swift
@@ -75,6 +75,13 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+
+        // To Add: Instantiate EntryViewController
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task = tasks[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+
     }
 }
 
